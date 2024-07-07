c252b0bbd9dbaabe642a65773a7465a6ebfae39f
Jose <Josey@duck.com>
2024-07-07 16:17:33 -0400

    I adjusted the sequence

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,13 @@ const date = $("#date");
 const text = $("#text");
 const save = $("#save");
 
-// #to-do, #in-progress, #done
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+    return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
+}
 
-function saveTask() {
+// Todo: create a function to create a task card
+function createTaskCard(task) {
     const info = {
         task: task.val.trim(),
         date: date.val.trim(),
@@ -17,37 +21,28 @@ function saveTask() {
     localStorage.setItem('info', JSON.stringify(info));
 }
 
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
-    return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
-}
-
-// Todo: create a function to create a task card
-function createTaskCard(task) {
-
-
-}
-
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-const taskList = JSON.parse(localStorage.getItem('info'));
-if (taskList !== null)
-    $("task") = info.task
-    $("date") = info.date
-    $("text") = info.text
+    const taskList = JSON.parse(localStorage.getItem('info'));
+    if (taskList === null) {
+        $("task") = info.task,
+        $("date") = info.date,
+        $("text") = info.text,
+}
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
+// #to-do, #in-progress, #done
 function handleDrop(event, ui) {
 
 }
