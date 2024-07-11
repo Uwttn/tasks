ad231ad512e0bc4233a2ff9a344def8132a6a765
Jose <Josey@duck.com>
2024-07-11 19:54:21 -0400

    Working on it again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,40 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-const task = $("#task");
-const date = $("#date");
-const text = $("#text");
-const save = $("#save");
 
 // Todo: create a function to generate a unique task id
+// 'Math.random()' generates a random floating-point number between 0 & 1 exclusively
+// `.toString(36)` is a string of 36 characters'
+// `substring` extracts from base 36-base string; starting from 2-9
+// `task-` prefix the unique string with task
 function generateTaskId() {
-    return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
+    return 'task-' + Math.random().toString(36).substring(2, 9);
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    const info = {
-        task: task.val.trim(),
-        date: date.val.trim(),
-        text: text.val.trim(),
-    };
-    localStorage.setItem('info', JSON.stringify(info));
+    const modal = document.getElementById('formModal')
+    const task = modal.querySelector('#task').value;
+    const date = modal.querySelector('#date').value;
+    const description = modal.querySelector('#description').value;
+
+    const card = createTask(task, date, description);
+    document.getElementsByClassName('card-body').appendchild('todo-cards');
+    // const info = {
+    //     task: task.val.trim(),
+    //     date: date.val.trim(),
+    //     text: text.val.trim(),
+    // };
+    // localStorage.setItem('info', JSON.stringify(info));
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-    var elem =document.createElement("span")
-    $(elem).data("task");
-    $(elem).data("date");
-    $(elem).data("text");
+
+    // var elem =document.createElement("span")
+    // $(elem).data("task");
+    // $(elem).data("date");
+    // $(elem).data("text");
 }
 
 // Todo: create a function to handle adding a new task
